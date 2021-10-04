7fec3f45e3cdad1a406452d784a1cc009947617f
gulatipayal <gulatipayal@yahoo.co.in>
2021-10-04 21:44:47 +0530

    user with id seen on local host

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def index
     render plain: User.order(:name).map {|user| user.to_pleasant_string}.join("\n")
   end
+  def show
+    id = params[:id]
+    render plain: "the user with id u want was #{id}"
+  end
+
 end
