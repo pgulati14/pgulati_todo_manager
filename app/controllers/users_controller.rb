0555c0313c378e35f84df1e0d020062f031b212c
gulatipayal <gulatipayal@yahoo.co.in>
2021-10-04 21:40:36 +0530

    userss displayable format, new lines , ordered alphabetically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # users_controller.rb
 class UsersController < ApplicationController
   def index
-    render plain: User.all.to_a
+    render plain: User.order(:name).map {|user| user.to_pleasant_string}.join("\n")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-
-
+  def to_pleasant_string
+    "#{id}. #{name} #{email} "
   end
+end
