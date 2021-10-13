7de6c5c0c1dc4811bd3b932b060ae2ab740c99fe
payal gulati <gulatipayal@yahoo.co.in>
2021-10-14 00:53:58 +0530

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,15 @@ class UsersController < ApplicationController
     render plain: User.order(:name).map {|user| user.to_pleasant_string}.join("\n")
   end
   def create
+    #render "User Created"
     name=params[:name]
     email=params[:email]
     password=params[:password]
-    #render plain: "hey! the params are #{params.to_s}"
+   # render plain: "hey! the params are #{params.to_s}"
     new_user = User.create!(
-      name: name,
-      email: email,
-      password: password,
+     name: name,
+     email: email,
+     password: password,
     )
     response_text= "New user created with id #{new_user.id}"
     render plain: response_text
@@ -29,3 +30,4 @@ class UsersController < ApplicationController
     user = User.find_by(email: email, password: password)
     render plain: user.present?
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post "users/create", to:"users#create"
+  post "users/login", to:"users#login"
   resources :users
   resources :todos
 end
